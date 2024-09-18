0025fdfec99f022b652a375584523e1a9c99dd49
Denis Malinin <melowetty@mail.ru>
2024-09-18 12:35:17 +0500

    add deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,17 @@ plugins {
 
 group = "ru.melowetty"
 version = "1.0-SNAPSHOT"
+val ktor_version = "2.3.12"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation(kotlin("test"))
 }
 

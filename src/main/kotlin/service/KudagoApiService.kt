0d0b0c89df087b8e34341596ae5417a9a1f1dff8
Denis Malinin <melowetty@mail.ru>
2024-09-25 14:12:08 +0500

    add exception handling in kudago api service

--- src/main/kotlin/service/KudagoApiService.kt
+++ src/main/kotlin/service/KudagoApiService.kt
@@ -2,14 +2,20 @@ package ru.melowetty.service
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.get
+import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import ru.melowetty.model.KudagoResponse
 import ru.melowetty.model.News
 
 class KudagoApiService(
     private val httpClient: HttpClient
 ) {
+    private val logger = KotlinLogging.logger {  }
+
     private val fields =
         listOf("id", "title", "publication_date", "place", "description", "site_url", "favorites_count", "comments_count")
 
@@ -18,26 +24,45 @@ class KudagoApiService(
         return sequence {
             var nextPage = page
             while (true) {
-                val news = getNewsByPage(nextPage, count)
-                if (news.isEmpty()) break
-                yieldAll(news)
-                nextPage += 1
+                try {
+                    val news = getNewsByPage(nextPage, count)
+                    if (news.isEmpty()) break
+                    yieldAll(news)
+                    nextPage += 1
+                } catch (runtimeException: RuntimeException) {
+                    logger.error { runtimeException }
+                    break
+                }
             }
         }
     }
 
     private fun getNewsByPage(page: Int, count: Int): List<News> {
-        val response = runBlocking<KudagoResponse> {
-            httpClient.get("https://kudago.com/public-api/v1.4/news") {
-                url {
-                    parameters.append("location", "spb")
-                    parameters.append("fields", fields.joinToString(","))
-                    parameters.append("expand", "place")
-                    parameters.append("page_size", count.toString())
-                    parameters.append("page", page.toString())
-                }
-            }.body()
+        try {
+            val response = runBlocking<KudagoResponse> {
+                httpClient.get("https://kudago.com/public-api/v1.4/news") {
+                    url {
+                        parameters.append("location", "spb")
+                        parameters.append("fields", fields.joinToString(","))
+                        parameters.append("expand", "place")
+                        parameters.append("page_size", count.toString())
+                        parameters.append("page", page.toString())
+                    }
+                }.body()
+            }
+            return response.results
+        } catch (serverResponseException: ServerResponseException) {
+            logger.error { serverResponseException }
+            throw RuntimeException("Ошибка во время получения ответа для получения новостей")
+        } catch (clientRequestException: ClientRequestException) {
+            logger.error { clientRequestException }
+            throw RuntimeException("Ошибка во время отправки запроса для получения новостей")
+        } catch (ioException: IOException) {
+            logger.error { ioException }
+            throw RuntimeException("Проблема с интернетом")
+        } catch (exception: Exception) {
+            logger.error { exception }
+            throw RuntimeException("Произошла ошибка во время получения новостей")
         }
-        return response.results
     }
 }
